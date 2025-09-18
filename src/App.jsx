1f68e6c5fab72c6b823badb36a30e38d963ac380
joCodes <76087277+joCodes01@users.noreply.github.com>
2025-09-18 15:34:15 +1000

    add tailwind to React app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Change to main</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const [todos, setTodos] = useState([
+    {id: 1, task: "Feed the dog", important: false},
+    {id: 2, task: "Wash the car", important: false},
+    {id: 3, task: "Eat the brownies", important: false},
+    {id: 4, task: "Run a bubble bath", important: false},
+    {id: 5, task: "Meditation", important: false},
+    {id: 6, task: "Homework", important: false},
+
+  ]);
+
+  return(
+
+    <div>
+      <h1>todo list next level</h1>  
+      <ul>
+        {todos.map((todo)=> <li>{todo.task}</li>)}
+      </ul>
+    </div>
   )
+
 }
 
 export default App
